8f924b2d7fa058ed164b5746a696901f7c0c9ffc
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-26 14:41:56 +0200

    Update OutdatedSubState.hx

--- source/OutdatedSubState.hx
+++ source/OutdatedSubState.hx
@@ -21,11 +21,15 @@ class OutdatedSubState extends MusicBeatState
 			+ Application.current.meta.get('version')
 			+ " while the most recent version is "
 			+ "v0.2.8" // might use the GitHub API later to check the version based on NinjaMuffin99's latest tag
-			+ "! Press Space to go to itch.io, or ESCAPE to ignore this!!",
+			+ "! Press Enter to go to itch.io, or ESCAPE to ignore this!!",
 			32);
 		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		txt.screenCenter();
 		add(txt);
+
+                #if android
+	        addVirtualPad(NONE, A_B);
+                #end
 	}
 
 	override function update(elapsed:Float)
